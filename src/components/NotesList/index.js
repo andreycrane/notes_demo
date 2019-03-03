86df1138a74b2ea1b07443897bbe50587c542705
Andrey Zhuravlyov <crane.andrey@gmail.com>
2019-03-03 14:05:36 +0300

    добавил фильтрацию заметок по категории

--- src/components/NotesList/index.js
+++ src/components/NotesList/index.js
@@ -1,18 +1,50 @@
 // @flow
 
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import type { Location } from 'react-router-dom';
 
-import type { TState, TId } from '../../types';
+import type {
+  TState,
+  TId,
+  TNote,
+  TNotes,
+} from '../../types';
+
 import { removeNote } from '../../ducks/notes';
 
 import NotesList from './components';
 
-const mapStateToProps = (state: TState): mixed => ({
-  notes: state.notes,
-});
+type TProps = {
+  location: Location
+};
+
+function mapStateToProps(state: TState, ownProps: TProps): mixed {
+  const { location } = ownProps;
+  const sp = new URLSearchParams(location.search);
+
+  const filterParams = [
+    function categoryFilter(n: TNote): boolean {
+      if (!sp.has('category')) {
+        return true;
+      }
+
+      return (n.category === sp.get('category'));
+    },
+  ];
+
+
+  const notes = filterParams.reduce(
+    ((acc: TNotes, filterFunc): TNotes => acc.filter(filterFunc)),
+    state.notes,
+  );
+
+
+  return ({ notes });
+}
 
 const mapDispatchToProps = (dispatch): mixed => ({
   remove: (id: TId): void => dispatch(removeNote({ id })),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(NotesList);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NotesList));
